3867c9e127196a89a4660186e5c642dadc4dc0f5
amihut <amihut@EN610723>
2020-06-03 17:46:34 +0300

    REFACTORING - change function into arrow functions

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,15 +1,14 @@
 const fs = require( 'fs' )
+const chalk = require( 'chalk' )
 
-const getNotes = function() {
+const getNotes = () => {
   return 'Your notes...'
 }
 
-const addNote = function( title, body ) {
+const addNote = ( title, body ) => {
   const notes = loadNotes()
 
-  const duplicateNotes = notes.filter( function( note ) {
-    return note.title === title
-  } )
+  const duplicateNotes = notes.filter( ( note ) => note.title === title )
 
   if ( duplicateNotes.length === 0 ) {
     notes.push( {
@@ -17,28 +16,31 @@ const addNote = function( title, body ) {
       body: body
     } )
     saveNotes( notes )
-    console.log( 'New note added!' )
+    console.log( chalk.green.inverse( 'New note added!' ) )
   } else {
-    console.log( 'Note title taken!' )
+    console.log( chalk.red.inverse( 'Note title taken!' ) )
   }
 }
 
-const removeNote = function( title ) {
+const removeNote = ( title ) => {
   const notes = loadNotes()
 
-  const notesToKeep = notes.filter( function( note ) {
-    return note.title !== title
-  } )
+  const notesToKeep = notes.filter( ( note ) => note.title !== title )
 
-  saveNotes( notesToKeep )
+  if ( notes.length > notesToKeep.length ) {
+    console.log( chalk.green.inverse( 'Note removed!' ) )
+    saveNotes( notesToKeep )
+  } else {
+    console.log( chalk.red.inverse( 'No note found!' ) )
+  }
 }
 
-const saveNotes = function( notes ) {
+const saveNotes = ( notes ) => {
   const dataJSON = JSON.stringify( notes )
   fs.writeFileSync( 'notes.json', dataJSON )
 }
 
-const loadNotes = function() {
+const loadNotes = () => {
   try {
     const dataBuffer = fs.readFileSync( 'notes.json' )
     const dataJSON = dataBuffer.toString()
