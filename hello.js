c7e85241d1be3797a1141263f6ed86e319629331
amihut <amihut@EN610723>
2020-06-03 16:15:42 +0300

    add and remove note functionality

--- hello.js
+++
@@ -1,1 +0,0 @@
-console.log( 'welocme to the class!' );
--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,64 @@
-const validator = require( 'validator' )
-const getNotes = require( './notes.js' )
+const chalk = require( 'chalk' )
+const yargs = require( 'yargs' )
 
-const msg = getNotes()
+const notes = require( './notes.js' )
 
-console.log( msg )
+//Customize yargs version
+yargs.version( '1.1.0' )
+
+//Create add command
+yargs.command( {
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function( argv ) {
+    notes.addNote( argv.title, argv.body )
+  }
+} )
+
+//Create remove command
+yargs.command( {
+  command: 'remove',
+  describe: 'Remove a note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function( argv ) {
+    notes.removeNote( argv.title )
+  }
+} )
+
+//Create list command
+yargs.command( {
+  command: 'list',
+  describe: 'List your notes',
+  handler: function() {
+    console.log( 'List out all your notes' )
+  }
+} )
+
+//Create read command
+yargs.command( {
+  command: 'read',
+  describe: 'Read a note',
+  handler: function() {
+    console.log( 'Reading a note' )
+  }
+} )
+
+yargs.parse()
