4719c82469b32c199833dd1a508de33758bdd67c
amihut <amihut@EN610723>
2020-06-05 16:59:27 +0300

    add dynamic pages with handlebars

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,12 +1,32 @@
 const path = require( 'path' )
-
 const express = require( 'express' )
 
 const app = express()
 
 const publicDirectoryPath = path.join( __dirname, '../public' )
 
+app.set( 'view engine', 'hbs' )
 app.use( express.static( publicDirectoryPath ) )
+
+app.get( '', ( req, res ) => {
+  res.render( 'index', {
+    title: 'Weather',
+    name: 'Patricia Mihut'
+  } )
+} )
+
+app.get( '/about', ( req, res ) => {
+  res.render( 'about', {
+    title: 'About Me',
+    name: 'Patricia Mihut'
+  } )
+} )
+
+app.get( '/help', ( req, res ) => {
+  res.render( 'help', {
+    helpText: 'This is some helpful text.'
+  } )
+} )
 
 app.get( '/weather', ( req, res ) => {
   res.send( {
