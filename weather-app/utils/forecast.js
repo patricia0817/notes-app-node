7019b511e19ed21cca100507fed9c97670244d57
amihut <amihut@EN610723>
2020-06-05 12:23:51 +0300

    refactoring

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -3,13 +3,13 @@ const request = require( 'request' )
 const forecast = ( latitude, longitude, callback ) => {
   const url = 'http://api.weatherstack.com/current?access_key=290822359e8baddc66c29a2a5ee6380e&query=' + latitude + ',' + longitude + '&units=m'
 
-  request( { url: url, json: true }, ( error, response ) => {
+  request( { url, json: true }, ( error, { body } ) => {
     if ( error ) {
       callback( 'Unable to connect to weather service!', undefined )
-    } else if ( response.body.error ) {
+    } else if ( body.error ) {
       callback( 'Unable to find location', undefined )
     } else {
-      callback( undefined, response.body.current.weather_descriptions[ 0 ] + " It's curretly " + response.body.current.temperature + " degrees out.It feels like " + response.body.current.feelslike + " degrees out." )
+      callback( undefined, body.current.weather_descriptions[ 0 ] + " It's curretly " + body.current.temperature + " degrees out.It feels like " + body.current.feelslike + " degrees out." )
     }
   } )
 
--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -2,16 +2,16 @@ const request = require( 'request' )
 
 const geocode = ( address, callback ) => {
   url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent( address ) + '.json?access_token=pk.eyJ1IjoicGF0cmljaWFtaWh1dCIsImEiOiJja2Iwa3JhZDUwOHlzMnZzOTE0eHI0NWxsIn0.kWVFFFTEHZX9z61W7_9Rug&limit=1'
-  request( { url: url, json: true }, ( error, response ) => {
+  request( { url, json: true }, ( error, { body: { features } } ) => {
     if ( error ) {
       callback( 'Unable to connect to location services!', undefined )
-    } else if ( response.body.features.length === 0 ) {
+    } else if ( features.length === 0 ) {
       callback( 'Unable to find location. Try another search.', undefined )
     } else {
       callback( undefined, {
-        latitude: response.body.features[ 0 ].center[ 1 ],
-        longitude: response.body.features[ 0 ].center[ 0 ],
-        location: response.body.features[ 0 ].place_name
+        latitude: features[ 0 ].center[ 1 ],
+        longitude: features[ 0 ].center[ 0 ],
+        location: features[ 0 ].place_name
       } )
     }
   } )
