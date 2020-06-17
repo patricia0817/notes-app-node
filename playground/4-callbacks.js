7e1538ea62879966eac61de8e4a846b05d938843
amihut <amihut@EN610723>
2020-06-17 14:56:12 +0300

    promisses practice

--- playground/4-callbacks.js
+++ playground/4-callbacks.js
@@ -1,32 +1,14 @@
-setTimeout( () => {
-  console.log( 'Two seconds are up' )
-}, 2000 )
-
-const names = [ 'Patry', 'Tony', 'Dari', 'Dei' ]
-const shortNames = names.filter( name => {
-  return name.length <= 4
-} );
-
-const geocode = ( adress, callback ) => {
+const doWorkCallback = ( callback ) => {
   setTimeout( () => {
-    const data = {
-      latitude: 0,
-      longitude: 0
-    }
-    callback( data )
+    // callback( 'This is my error!', undefined )
+    callback( undefined, [ 1, 4, 7 ] )
   }, 2000 )
 }
 
-geocode( 'Philadelphia', ( data ) => {
-  console.log( data )
-} )
+doWorkCallback( ( error, result ) => {
+  if ( error ) {
+    return console.log( error )
+  }
 
-const add = ( a, b, callback ) => {
-  setTimeout( () => {
-    callback( a + b )
-  }, 2000 )
-}
-
-add( 1, 4, ( sum ) => {
-  console.log( sum )
+  console.log( result )
 } )
