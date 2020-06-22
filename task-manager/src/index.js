6e10c9c41bdb8c4134f13ed7296fe793d77634dd
amihut <amihut@EN610723>
2020-06-22 17:24:25 +0300

    resource updating endpoints

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -42,6 +42,27 @@ app.get( '/users/:id', async ( req, res ) => {
   }
 } )
 
+app.patch( '/users/:id', async ( req, res ) => {
+  const _id = req.params.id
+  const allowedUpdates = [ 'name', 'email', 'password', 'age' ]
+  const updates = Object.keys( req.body )
+  const isValidOperation = updates.every( ( update ) => allowedUpdates.includes( update ) )
+
+  if ( !isValidOperation ) {
+    return res.status( 400 ).send( { error: 'Invalid updates!' } )
+  }
+
+  try {
+    const user = await User.findByIdAndUpdate( _id, req.body, { new: true, runValidators: true } )
+    if ( !user ) {
+      return res.status( 404 ).send()
+    }
+    res.send( user )
+  } catch ( e ) {
+    res.status( 400 ).send( e )
+  }
+} )
+
 app.post( '/tasks', async ( req, res ) => {
   const task = new Task( req.body )
 
@@ -74,6 +95,27 @@ app.get( '/tasks/:id', async ( req, res ) => {
     res.send( task )
   } catch ( e ) {
     res.status( 500 ).send()
+  }
+} )
+
+app.patch( '/tasks/:id', async ( req, res ) => {
+  const _id = req.params.id
+  const allowedUpdates = [ 'description', 'completed' ]
+  const updates = Object.keys( req.body )
+  const isValidOperation = updates.every( ( update ) => allowedUpdates.includes( update ) )
+
+  if ( !isValidOperation ) {
+    return res.status( 400 ).send( { error: 'Invalid updates!' } )
+  }
+
+  try {
+    const task = await Task.findByIdAndUpdate( _id, req.body, { new: true, runValidators: true } )
+    if ( !task ) {
+      return res.status( 404 ).send()
+    }
+    res.send( task )
+  } catch ( e ) {
+    res.status( 400 ).send( e )
   }
 } )
 
